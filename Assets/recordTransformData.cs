8c1fe1dd5eef17f6735aa8ad70bd2461f4016aa0
VRLAB1\Liadlab <hirschhorn@mail.tau.ac.il>
2022-06-16 20:23:29 +0300

    updating assets

---
+++ Assets/recordTransformData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recordTransformData : MonoBehaviour
+{
+    [SerializeField] Transform transformRef; // object that points forward and that is reference to head and gaze
+    [SerializeField] Vector3 relativePosition;
+    [SerializeField] float gazeAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // calculates head position relative to transformReference which should be placed in player start position.
+        relativePosition = transformRef.position - transform.position;
+
+        // calculate gaze angle
+        Vector3.Dot(transformRef.forward, transform.forward);
+
+    }
+}
